faedb4f7c90c0b55e5d747254c2e684b74ac5551
Marcilio Correia <msc_marcilio@hotmail.com>
2021-01-07 15:31:51 +0000

    project update with patterns

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,13 +6,26 @@ class FormularioCadastro extends Component {
     this.titulo = "";
     this.texto = "";
     this.categoria = "Sem Categoria";
+    this.state = {categorias:[]}
+
+    this._novasCategorias = this._novasCategorias.bind(this);
   }
 
+  componentDidMount(){
+    this.props.categorias.inscrever( this._novasCategorias);
+    
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever( this._novasCategorias);
+  }
+  _novasCategorias(categorias){
+    this.setState({...this.state, categorias})
+  }
   _handleMudancaCategoria(evento){
     evento.stopPropagation();
     this.categoria = evento.target.value;
   }
-  // parei no vídeo 03 do bloco 03
   _handleMudancaTitulo(evento) {
     evento.stopPropagation();
     this.titulo = evento.target.value;
@@ -38,8 +51,8 @@ class FormularioCadastro extends Component {
         >
           <option>Sem Categoria</option>
 
-          {this.props.categorias.map((categoria) => {
-            return <option>{categoria}</option>;
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index} >{categoria}</option>;
           })}
         </select>
         <input
